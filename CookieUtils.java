723de6d6ead18306b1d70182f365de59ad60aad1
dev-ikae <98100047+devikae@users.noreply.github.com>
2022-04-19 19:02:37 +0900

    Add files via upload

---
+++ CookieUtils.java
@@ -0,0 +1,73 @@
+package com.study.common.util;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+
+
+public class CookieUtils {
+	//특정 쿠키가 검색, 쿠키생성+set 한번에 하기 
+	
+	private Map<String, Cookie> cookieMap = new HashMap<String, Cookie>();
+	
+	// 생성자, CookieUtils 만들 때 매개변수로 request를 받아서 cookieMap에 저장
+	public CookieUtils(HttpServletRequest request) { // 특정 키 유무 리퀘스트로 판별
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null) {
+			for(Cookie cookie : cookies) {
+				this.cookieMap.put(cookie.getName(), cookie);
+			}
+		}
+		
+	}
+	
+	// 해당 쿠키가 있는지 없는지, 
+	public boolean exists(String name) { 
+		return cookieMap.get(name)!=null; 
+	}
+	
+	// 해당 쿠키 얻기 
+	public Cookie getCookie(String cookieName) {
+		return cookieMap.get(cookieName);
+	}
+	
+	public String getValue(String name) throws IOException{
+		Cookie cookie=cookieMap.get(name);
+		if(cookie==null) return null;
+		return URLDecoder.decode(cookie.getValue(),"utf-8");
+	}
+	
+	// 쿠키생성과 set을 한번에
+	
+	public static Cookie createCookie(String name, String value) throws IOException {
+		return createCookie(name, value, "", "", -1);
+	}
+	
+	public static Cookie createCookie(String name,String value,String path) throws IOException{
+		return createCookie(name,value,"",path,-1);
+	}
+	
+	
+	public static Cookie createCookie(String name,String value,int maxAge) throws IOException{
+		return createCookie(name,value,"","",maxAge);
+	}
+	
+	public static Cookie createCookie(String name, String value, String path, int maxAge) throws IOException {
+		return createCookie(name, value, "", path, maxAge);
+	}
+	
+	public static Cookie createCookie(String name, String value, String domain, String path, int maxAge) throws IOException {
+		Cookie cookie = new Cookie(name, URLEncoder.encode(value, "utf-8"));
+		cookie.setDomain(domain);
+		cookie.setPath(path);
+		cookie.setMaxAge(maxAge);
+		return cookie;
+	}
+	
+}
